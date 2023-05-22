74952f71ccaf5e94554b023c37c6ab8445278d63
huangshuaishuai <1351090471@qq.com>
2023-05-22 10:43:28 +0800

    update

--- kgdoctor_parent/kgdoctor_seckill/src/main/java/com/hss/kgdoctor/constants/RedisKeyConstants.java
+++
@@ -1,8 +0,0 @@
-package com.hss.kgdoctor.constants;
-
-public class RedisKeyConstants {
-    public static final String SKEKILL_DOCTOR_KEY = "cache:seckill:doctor:info:";
-    public static final String SKEKILL_STOCK_KEY = "cache:seckill:doctor:stock:";
-
-    public static final String SECKILL_ORDER_SET = "cache:seckill:orderset:";
-}
